15bd309bb397c15e636336eb32f18acab5e83a78
Dino Danic <dino.danic@gmail.com>
2022-03-04 21:58:40 +0100

    1

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,9 +9,27 @@ module Types
 
     # TODO: remove me
     field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+      description: "An example field added by the generator" do
+        argument :name, String, required: true
+      end
+    def test_field(name:)
+      "Hello #{name}!"
     end
+
+    field :author, Types::AuthorType, null: true do
+      argument :id, ID, required: true
+
+    end
+
+    def author(id:)
+      Author.where(id: id).first
+    end
+
+    field :authors, [Types::AuthorType], null: false
+
+    def authors
+      Author.all
+    end
+
   end
 end
